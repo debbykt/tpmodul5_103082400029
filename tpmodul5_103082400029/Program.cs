c35329cc8c959a4d7515ba8a9712497f8d52bd12
debbykt <debbytampubolon24@gmail.com>
2026-03-26 01:02:27 -0700

    commit pertama

---
+++ tpmodul5_103082400029/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_103082400029
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
