a80d87b6df3d77695eb2d9a97c9de2826e81bb63
debbykt <debbytampubolon24@gmail.com>
2026-03-26 19:51:07 -0700

    menambahkan class DataGeneric

--- tpmodul5_103082400029/Program.cs
+++ tpmodul5_103082400029/Program.cs
@@ -10,6 +10,37 @@ namespace tpmodul5_103082400029
     {
         static void Main(string[] args)
         {
+
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUser<string>("Debby");
+
+            DataGeneric<string> data = new DataGeneric<string>("103082400029");
+            data.PrintData();
+             
         }
     }
+
+    class HaloGeneric
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine("Halo user " + user);
+        }
+    }
+
+    class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah : " + data);
+        }
+    }
+
 }
